368748fd7b66f09d1c481139e31aa0d316476991
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-11-07 15:23:38 +0800

    first commit

---
+++ cgpa/cgpa.py
@@ -0,0 +1,4 @@
+cgpa = float(input("Enter your CGPA: "))
+
+if(cgpa > 1.8 and cgpa <= 4.0 and cgpa > 0):
+    print("You passed!")
---
+++ grade/grade.py
@@ -0,0 +1,6 @@
+if "__main__" == __name__:
+    grade = int(input("Please enter your grade: "))
+    if grade < 0 or grade > 100:
+        print("Invalid grade")
+    
+    print("Thank You!")
